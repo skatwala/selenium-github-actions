e13feea42188dd48ac0524e9f6521f03460cafbf
Saptarshi <sknj24@gmail.com>
2021-03-14 20:43:14 -0500

    Update UserLoginTest.java

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -5,6 +5,7 @@ public class UserLoginTest extends TestBase {
 
     @Test
     public void userLogin() {
+        System.out.println("Current URL is:" + driver.getCurrentUrl());
         loginPage= new LoginPageObject(driver);
         loginPage.typeEmailId(username);
         loginPage.typePassword(password);
